28e0e5e954692645e9044a5907346adf208b168e
anakapanadzeklein <anakapanadzeklein@gmail.com>
2025-02-22 12:55:01 +0400

    uploaded finished project

---
+++ app.js
@@ -0,0 +1,132 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const multer = require('multer');
+const fs = require('fs');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI = 'mongodb+srv://anakapanadzeklein:dtsCKf7cV26cqFta@myproject.amlw6.mongodb.net/shop?retryWrites=true&w=majority&appName=MyProject';
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session ? req.session.isLoggedIn : false;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      next(new Error(err));
+    });
+});
+
+app.get('/invoice/:orderId', (req, res, next) => {
+  const orderId = req.params.orderId; 
+  console.log('Order ID:', orderId); 
+
+  const invoicePath = path.join(__dirname, 'data', 'invoices', `invoice-${orderId}.pdf`);
+  console.log('Invoice Path:', invoicePath);  
+
+  fs.access(invoicePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      return next(new Error('Invoice not found!'));
+    }
+
+    res.sendFile(invoicePath);
+  });
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.get('/500', errorController.get500);
+
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  console.error(error); 
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session ? req.session.isLoggedIn : false
+  });
+});
+
+mongoose.connect(MONGODB_URI)
+  .then(result => {
+    const port = 4000;
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
